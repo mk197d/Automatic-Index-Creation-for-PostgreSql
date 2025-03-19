392df949326c41bc44e02fdc23c5317985266067
mk197d <mayank8197d@gmail.com>
2025-03-19 11:39:43 +0530

    added basic code for query processing

--- code/main.cpp
+++ code/main.cpp
@@ -1,32 +1,84 @@
 #include <iostream>
 #include <pqxx/pqxx> // PostgreSQL C++ library
+#include <string>
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
+using namespace pqxx;
+
+void printResult(result &R) {
+    if (R.empty()) {
+        cout << "Query executed successfully. No results to display." << endl;
+        return;
+    }
+
+    for (int i = 0; i < R.columns(); ++i) {
+        cout << setw(15) << left << R.column_name(i);
+    }
+    cout << endl;
+
+    for (int i = 0; i < R.columns(); ++i) {
+        cout << setw(15) << setfill('-') << "";
+    }
+    cout << setfill(' ') << endl;
+
+    for (const auto &row : R) {
+        for (int i = 0; i < R.columns(); ++i) {
+            cout << setw(15) << left << row[i].c_str();
+        }
+        cout << endl;
+    }
+}
+
+void executeAndPrintQuery(connection &C, const string &query) {
+    try {
+        cout<<query<<endl;
+        nontransaction N(C);
+        result R = N.exec(query);
+        printResult(R);
+
+    } catch (const exception &e) {
+        cerr << "Error executing query: " << e.what() << endl;
+    }
+}
 
 int main() {
+    string hostname = "localhost";
+    string port = "5432";
+    string database = "ecommerce";
+    string username = "";
+    string password = "";
+
     try {
-        pqxx::connection C("dbname=ecommerce user=sakshamrathi password=12345678 host=localhost port=5432");
-        if (C.is_open()) {
-            std::cout << "Opened database successfully: " << C.dbname() << std::endl;
-        } else {
-            std::cerr << "Can't open database" << std::endl;
+        connection C("dbname=" + database + " user=" + username + " password=" + password +
+                     " host=" + hostname + " port=" + port);
+        if (!C.is_open()) {
+            cerr << "Can't open database" << endl;
             return 1;
         }
 
-        // pqxx::work W(C);
-        
-        // Query data
-        pqxx::nontransaction N(C);
-        pqxx::result R = N.exec("SELECT * FROM users;");
-        
-        for (const auto& row : R) {
-            std::cout << "ID: " << row["user_id"].as<int>()<< std::endl;
+        string cmd;
+        while (true) {
+            cout << "pgshell# ";
+            getline(cin, cmd);
+
+            if (cmd == "\\q") {
+                cout << "Exiting..." << endl;
+                break;
+            }
+
+            if (cmd.empty()) {
+                continue;
+            }
+
+            executeAndPrintQuery(C, cmd);
         }
 
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
+    } catch (const exception &e) {
+        cerr << e.what() << endl;
         return 1;
     }
-    
+
     return 0;
 }
