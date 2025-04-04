152229260baea663e70624c00e2033cdad1f3deb
Saksham Rathi <22b1003@iitb.ac.in>
2025-04-04 15:52:11 +0530

    small-changes

--- code/memory.h
+++ code/memory.h
@@ -1,5 +1,15 @@
 #ifndef MEMORY_H
 #define MEMORY_H
+#include "common-headers.h"
+#include "keywords.h"
 
+
+struct IndexEntry {
+    std::string tableName;
+    std::string attributeName;
+    std::string indexName;
+    int createTime;
+    int num_accesses;
+};
 
 #endif
