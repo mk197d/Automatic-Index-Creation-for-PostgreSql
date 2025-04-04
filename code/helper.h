7d9f1e20be1d2f8bed6270a804d84d65fcf57ce2
Saksham Rathi <22b1003@iitb.ac.in>
2025-04-04 14:57:20 +0530

    small-chanes

--- code/helper.h
+++ code/helper.h
@@ -1,1 +1,4 @@
 #include "common-headers.h"
+
+void printResult(pqxx::result &R);
+void executeAndPrintQuery(pqxx::connection &C, const std::string &query);
--- code/main.cpp
+++ code/main.cpp
@@ -1,43 +1,9 @@
 #include "common-headers.h"
-#include "keywords.hh"
+#include "keywords.h"
+#include "helper.h"
 
 // using namespace std;
 // using namespace pqxx;
-
-void printResult(pqxx::result &R) {
-    if (R.empty()) {
-        std::cout << "Query executed successfully. No results to display." << std::endl;
-        return;
-    }
-
-    for (int i = 0; i < R.columns(); ++i) {
-        std::cout << std::setw(15) << std::left << R.column_name(i);
-    }
-    std::cout << std::endl;
-
-    for (int i = 0; i < R.columns(); ++i) {
-        std::cout << std::setw(15) << std::setfill('-') << "";
-    }
-    std::cout << std::setfill(' ') << std::endl;
-
-    for (const auto &row : R) {
-        for (int i = 0; i < R.columns(); ++i) {
-            std::cout << std::setw(15) << std::left << row[i].c_str();
-        }
-        std::cout << std::endl;
-    }
-}
-
-void executeAndPrintQuery(pqxx::connection &C, const std::string &query) {
-    try {
-        std::cout << query << std::endl;
-        pqxx::nontransaction N(C);
-        pqxx::result R = N.exec(query);
-        printResult(R);
-    } catch (const std::exception &e) {
-        std::cerr << "Error executing query: " << e.what() << std::endl;
-    }
-}
 
 int main() {
     std::string hostname = "localhost";
