eee010c8690cf5d53c44a3695fe686c94bc345e0
Saksham Rathi <22b1003@iitb.ac.in>
2025-04-30 10:54:05 +0530

    index

--- code/index.h
+++ code/index.h
@@ -3,7 +3,7 @@
 #include "keywords.h"
 #include "datastructures.hh"
 #define THRESHOLD1 10
-#define THRESHOLD2 100
+#define THRESHOLD2 60
 typedef std::pair<std::string,std::string> pair_t;
 typedef std::map<pair_t,int> matrix_t;
 extern std::map<std::string, KeywordType> keyword_map;
